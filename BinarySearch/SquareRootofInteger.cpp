1affe34824906653d9139830cd8aef8dabd30696
Shaurya Airi <shauryaairi.iitkgp@gmail.com>
2018-09-07 11:09:15 +0530

    Finally Completed Though

--- BinarySearch/SquareRootofInteger.cpp
+++ BinarySearch/SquareRootofInteger.cpp
@@ -1,1 +1,23 @@
+//https://www.interviewbit.com/problems/square-root-of-integer/
+int Solution::sqrt(int A) {
 
+    if(A<=1) return A;
+    else if(A>1&&A<4) return 1;
+    if(A==4) return 2;
+    int high=(A/2);
+    int low=1,res=0,mid;
+    // long int mid; 
+    while(low<=high){
+        mid = (high+low)/2;
+        if(mid<=A/mid) {//return mid;
+        // else if(mid*mid<A){ 
+            res=mid;
+            low=mid+1;}
+       // if(mid*mid<A) low=mid+1;
+        else high=mid-1;
+        // cout<<res<<endl;
+    }
+    return res;
+    
+    
+}
